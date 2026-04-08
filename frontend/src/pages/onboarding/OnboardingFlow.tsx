452e99d058e4a18c9a78e2f593a14d870885186b
Ricardo Cárcamo <ricardo.carcamo.cofre@gmail.com>
2026-04-08 06:24:22 -0400

    Fix unused import in OnboardingFlow

--- frontend/src/pages/onboarding/OnboardingFlow.tsx
+++ frontend/src/pages/onboarding/OnboardingFlow.tsx
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
-import { useTheme } from '../../context/ThemeContext';
 import api from '../../api/axios';
 
 const OnboardingFlow: React.FC = () => {
